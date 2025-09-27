e16db92c876afd64e6a49549e9177deffb4373df
Sue-Holding <sue.holding55@gmail.com>
2025-09-27 08:13:16 +0200

    commited out unused imports

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
-import EventDetails from "./EventDetails";
+// import EventDetails from "./EventDetails";
 
 export default function EventCard({ event }) {
   return (
